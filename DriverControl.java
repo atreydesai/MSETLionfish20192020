bb3388b3b6b4eb5e79ed26df10fd0512db090907
MatthewSong86 <54823079+MatthewSong86@users.noreply.github.com>
2019-10-26 17:20:36 -0700

    Update DriverControl.java

--- DriverControl.java
+++ DriverControl.java
@@ -17,7 +17,7 @@ public class DriverControl extends LinearOpMode{
         frontRight  = hardwareMap.get(DcMotor.class, "frontRight");
         backLeft  = hardwareMap.get(DcMotor.class, "backLeft");
         backRight  = hardwareMap.get(DcMotor.class, "backRight");
-        platform = hardwareMap.get(Servo.class, "platform");
+        //platform = hardwareMap.get(Servo.class, "platform");
         hooks1 = hardwareMap.get(Servo.class, "hooks1");
         hooks2 = hardwareMap.get(Servo.class, "hooks2");
 
@@ -25,8 +25,8 @@ public class DriverControl extends LinearOpMode{
         telemetry.update();
         frontLeft.setDirection(DcMotor.Direction.FORWARD);
         backLeft.setDirection(DcMotor.Direction.FORWARD);
-        frontRight.setDirection ( DcMotor.Direction.REVERSE);
-        backRight.setDirection(DcMotor.Direction.REVERSE);
+        frontRight.setDirection ( DcMotor.Direction.FORWARD);
+        backRight.setDirection(DcMotor.Direction.FORWARD);
         waitForStart();
         double fbLeftStick = 0;
         double lrLeftStick = 0;
@@ -34,24 +34,27 @@ public class DriverControl extends LinearOpMode{
         double fbRightStick = 0;
         double ppUp = 1; //subject to change
         double ppDown= 0 ; // subject to change
-        double hookUp = 1;//subject to change
-        double hookDown = 0;//subject to change
+        double hookUp = 0;//subject to change
+        double hookDown = 1;//subject to change
+        boolean rev = false;
         //put all movement code in here
         while(opModeIsActive()){
             //joystick control code
             fbLeftStick = -this.gamepad1.left_stick_y;
             fbRightStick = -this.gamepad1.right_stick_y;
             lrLeftStick = -this.gamepad1.left_stick_x; //if turning the wrong direction reverse the sign
-            lrRightStick = -this.gamepad1.right_stick_x; //if turning the wrong direction reverse the sign
-
+            lrRightStick = this.gamepad1.right_stick_x; //if turning the wrong direction reverse the sign
+            /*
             if(this.gamepad1.left_trigger > 0.1){
-                platformUp(ppUp); //move the platform up
+                hUp(ppUp); //move the platform up
 
             }
             else if(this.gamepad1.left_bumper == true){
                 platformDown(ppDown); //move the platform down
 
             }
+
+             */
             if(this.gamepad1.right_trigger > 0.1){
                 hUp(hookUp);
 
@@ -60,6 +63,20 @@ public class DriverControl extends LinearOpMode{
                 hDown(hookDown);
 
             }
+            /*
+            if(this.gamepad1.a == true){
+                if(rev ==true){
+                    //reverse(false);
+                    rev = false;
+                }
+                else if(rev ==false){
+                    //reverse(true);
+                    rev = true;
+                }
+
+            }
+
+             */
 
             //movingFB(fbLeftStick);
             //strafe(lrLeftStick);
@@ -70,7 +87,7 @@ public class DriverControl extends LinearOpMode{
             telemetry.update();
         }
     }
-    public void tank(double fbLeftStick, double fbRightStick){ //tank drive (one joystick controls one wheel)
+    public void tank(double fbLeftStick, double fbRightStick){ //tank drive (one joystick controls one side of wheels)
 
         frontLeft.setPower(fbLeftStick);
         backLeft.setPower(fbLeftStick);
@@ -111,10 +128,10 @@ public class DriverControl extends LinearOpMode{
     }
     public void turn(double lrRightStick){//turning in place
 
-        frontLeft.setPower(lrRightStick);
-        backLeft.setPower(-lrRightStick);
-        frontRight.setPower(lrRightStick);
-        backRight.setPower(-lrRightStick);
+        frontLeft.setPower(-lrRightStick);
+        backLeft.setPower(lrRightStick);
+        frontRight.setPower(-lrRightStick);
+        backRight.setPower(lrRightStick);
         telemetry.addData("Target Power", lrRightStick);
         telemetry.addData("Motor Power", frontLeft.getPower());
         telemetry.addData("Motor Power", frontRight.getPower());
@@ -122,7 +139,7 @@ public class DriverControl extends LinearOpMode{
         telemetry.addData("Motor Power", backRight.getPower());
     }
 
-    public void platformUp(double plat){//platform servo up
+    /*public void platformUp(double plat){//platform servo up
         platform.setPosition(plat);
         telemetry.addData("Target Power", plat);
         telemetry.addData("Servo Power", platform.getPosition());
@@ -132,20 +149,35 @@ public class DriverControl extends LinearOpMode{
         telemetry.addData("Target Power", plat);
         telemetry.addData("Servo Power", platform.getPosition());
     }
+
+     */
     public void hUp(double hook){//hook servo up
         hooks1.setPosition(hook);
-        hooks2.setPosition(hook);
-        telemetry.addData("Target Power", hook);
+        hooks2.setPosition(hook+1);
         telemetry.addData("Servo Power", hooks1.getPosition());
-        telemetry.addData("Servo Power", hooks2.getPosition ());
+        telemetry.addData("Servo Power", hooks2.getPosition());
 
     }
     public void hDown(double hook){//hook servo down
         hooks1.setPosition(hook);
-        hooks2.setPosition(hook);
-        telemetry.addData("Target Power", hook);
+        hooks2.setPosition(hook-1);
         telemetry.addData("Servo Power", hooks1.getPosition());
-        telemetry.addData("Servo Power", hooks2.getPosition ());
+        telemetry.addData("Servo Power", hooks2.getPosition());
     }
+    /*public void reverse(boolean rev){
+        if(rev == true){
+            frontLeft.setDirection(DcMotor.Direction.FORWARD);
+            backLeft.setDirection(DcMotor.Direction.FORWARD);
+            frontRight.setDirection ( DcMotor.Direction.FORWARD);
+            backRight.setDirection(DcMotor.Direction.FORWARD);
+        }
+        else if(rev == false){
+            frontLeft.setDirection(DcMotor.Direction.REVERSE);
+            backLeft.setDirection(DcMotor.Direction.REVERSE);
+            frontRight.setDirection ( DcMotor.Direction.REVERSE);
+            backRight.setDirection(DcMotor.Direction.REVERSE);
+        }
+
+    }*/
 
 }
