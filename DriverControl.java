975b09f0cdad4936d3eff94bd68739e6c671ce8a
MatthewSong86 <54823079+MatthewSong86@users.noreply.github.com>
2019-11-09 18:16:11 -0800

    Update DriverControl.java

--- DriverControl.java
+++ DriverControl.java
@@ -3,21 +3,22 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 
 
 @TeleOp(name="DriverControl", group="LinearOpmode")
 public class DriverControl extends LinearOpMode{
 
-    private DcMotor frontLeft, backLeft, frontRight, backRight;
-    private Servo hooks1,hooks2, platform;
+    private DcMotor frontLeft, backLeft, frontRight, backRight, claw, lift1, lift2;
+    private Servo hooks1,hooks2;
     public void runOpMode(){
         frontLeft  = hardwareMap.get(DcMotor.class, "frontLeft");
         frontRight  = hardwareMap.get(DcMotor.class, "frontRight");
         backLeft  = hardwareMap.get(DcMotor.class, "backLeft");
         backRight  = hardwareMap.get(DcMotor.class, "backRight");
-        //platform = hardwareMap.get(Servo.class, "platform");
+        claw = hardwareMap.get(DcMotor.class, "claw");
+        lift1 = hardwareMap.get(DcMotor.class, "lift1");
+        lift2 = hardwareMap.get(DcMotor.class, "lift2");
         hooks1 = hardwareMap.get(Servo.class, "hooks1");
         hooks2 = hardwareMap.get(Servo.class, "hooks2");
 
@@ -32,57 +33,41 @@ public class DriverControl extends LinearOpMode{
         double lrLeftStick = 0;
         double lrRightStick = 0;
         double fbRightStick = 0;
-        double ppUp = 1; //subject to change
-        double ppDown= 0 ; // subject to change
-        double hookUp = 0;//subject to change
-        double hookDown = 1;//subject to change
-        boolean rev = false;
+
         //put all movement code in here
         while(opModeIsActive()){
             //joystick control code
             fbLeftStick = -this.gamepad1.left_stick_y;
-            fbRightStick = -this.gamepad1.right_stick_y;
-            lrLeftStick = -this.gamepad1.left_stick_x; //if turning the wrong direction reverse the sign
             lrRightStick = this.gamepad1.right_stick_x; //if turning the wrong direction reverse the sign
-            /*
-            if(this.gamepad1.left_trigger > 0.1){
-                hUp(ppUp); //move the platform up
 
-            }
-            else if(this.gamepad1.left_bumper == true){
-                platformDown(ppDown); //move the platform down
-
-            }
-
-             */
             if(this.gamepad1.right_trigger > 0.1){
-                hUp(hookUp);
+                hUp();
 
             }
-            else if(this.gamepad1.right_bumper == true){
-                hDown(hookDown);
+            else if(this.gamepad1.right_bumper == true) {
+                hDown();
 
             }
-            /*
-            if(this.gamepad1.a == true){
-                if(rev ==true){
-                    //reverse(false);
-                    rev = false;
-                }
-                else if(rev ==false){
-                    //reverse(true);
-                    rev = true;
-                }
-
+            if(this.gamepad2.a==true){
+                clawOpen();
             }
+            else if(this.gamepad2.b==true){
+                clawOff();
+            }
+            if(this.gamepad2.dpad_up==true){
+                liftUp();
+            }
+            else if(this.gamepad2.dpad_down==true){
+                liftDown();
+            }
+            else if(this.gamepad2.dpad_right==true){
+                liftOff();
+            }
+            movingFB(fbLeftStick);
+            strafe(lrLeftStick);
+            turn(lrRightStick);
 
-             */
 
-            //movingFB(fbLeftStick);
-            //strafe(lrLeftStick);
-            //turn(lrRightStick);
-
-            tank(fbLeftStick, fbRightStick);
             telemetry.addData("Status", "Running");
             telemetry.update();
         }
@@ -113,19 +98,6 @@ public class DriverControl extends LinearOpMode{
         telemetry.addData("Motor Power", backLeft.getPower());
         telemetry.addData("Motor Power", backRight.getPower());
     }
-
-    public void strafe(double lrLeftStick){ //moving the root side to side w/o turning
-
-        frontLeft.setPower(lrLeftStick);
-        backLeft.setPower(-lrLeftStick);
-        frontRight.setPower(-lrLeftStick);
-        backRight.setPower(lrLeftStick);
-        telemetry.addData("Target Power", lrLeftStick);
-        telemetry.addData("Motor Power", frontLeft.getPower());
-        telemetry.addData("Motor Power", frontRight.getPower());
-        telemetry.addData("Motor Power", backLeft.getPower());
-        telemetry.addData("Motor Power", backRight.getPower());
-    }
     public void turn(double lrRightStick){//turning in place
 
         frontLeft.setPower(-lrRightStick);
@@ -138,46 +110,77 @@ public class DriverControl extends LinearOpMode{
         telemetry.addData("Motor Power", backLeft.getPower());
         telemetry.addData("Motor Power", backRight.getPower());
     }
+    public void strafe(double lrLeftStick){ //moving the root side to side w/o turning
 
-    /*public void platformUp(double plat){//platform servo up
-        platform.setPosition(plat);
-        telemetry.addData("Target Power", plat);
-        telemetry.addData("Servo Power", platform.getPosition());
-    }
-    public void platformDown(double plat){//platform servo down
-        platform.setPosition(plat);
-        telemetry.addData("Target Power", plat);
-        telemetry.addData("Servo Power", platform.getPosition());
+        frontLeft.setPower(lrLeftStick);
+        backLeft.setPower(-lrLeftStick);
+        frontRight.setPower(-lrLeftStick);
+        backRight.setPower(lrLeftStick);
+        telemetry.addData("Target Power", lrLeftStick);
+        telemetry.addData("Motor Power", frontLeft.getPower());
+        telemetry.addData("Motor Power", frontRight.getPower());
+        telemetry.addData("Motor Power", backLeft.getPower());
+        telemetry.addData("Motor Power", backRight.getPower());
     }
 
-     */
-    public void hUp(double hook){//hook servo up
-        hooks1.setPosition(hook);
-        hooks2.setPosition(hook+1);
+    public void clawOpen(){
+        claw.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        claw.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        int angle = 0;
+        claw.setTargetPosition (angle);
+        claw.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        claw.setPower(0.5);
+        while(claw.isBusy() && opModeIsActive()) {
+            claw.getCurrentPosition();
+        }
+        claw.setPower(0);
+    }
+    public void clawOff(){
+        claw.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        claw.setPower(0);
+        telemetry.addData("Target Power", 0);
+        telemetry.addData("Motor Power", claw.getPower());
+    }
+    public void liftUp(){
+        lift1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        lift2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        lift1.setPower(0.5);
+        lift2.setPower(0.5);
+        telemetry.addData("Target Power", 0.5);
+        telemetry.addData("Motor Power", lift1.getPower());
+        telemetry.addData("Motor Power", lift2.getPower());
+    }
+    public void liftDown(){
+        lift1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        lift2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        lift1.setPower(-0.5);
+        lift2.setPower(-0.5);
+        telemetry.addData("Target Power", -0.5);
+        telemetry.addData("Motor Power", lift1.getPower());
+        telemetry.addData("Motor Power", lift2.getPower());
+    }
+    public void liftOff(){
+        lift1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        lift2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        lift1.setPower(0);
+        lift2.setPower(0);
+        telemetry.addData("Target Power", 0);
+        telemetry.addData("Motor Power", lift1.getPower());
+        telemetry.addData("Motor Power", lift2.getPower());
+    }
+    public void hUp(){//hook servo up
+        hooks1.setPosition(0);
+        hooks2.setPosition(1);
         telemetry.addData("Servo Power", hooks1.getPosition());
         telemetry.addData("Servo Power", hooks2.getPosition());
 
     }
-    public void hDown(double hook){//hook servo down
-        hooks1.setPosition(hook);
-        hooks2.setPosition(hook-1);
+    public void hDown(){//hook servo down
+        hooks1.setPosition(1);
+        hooks2.setPosition(0);
         telemetry.addData("Servo Power", hooks1.getPosition());
         telemetry.addData("Servo Power", hooks2.getPosition());
     }
-    /*public void reverse(boolean rev){
-        if(rev == true){
-            frontLeft.setDirection(DcMotor.Direction.FORWARD);
-            backLeft.setDirection(DcMotor.Direction.FORWARD);
-            frontRight.setDirection ( DcMotor.Direction.FORWARD);
-            backRight.setDirection(DcMotor.Direction.FORWARD);
-        }
-        else if(rev == false){
-            frontLeft.setDirection(DcMotor.Direction.REVERSE);
-            backLeft.setDirection(DcMotor.Direction.REVERSE);
-            frontRight.setDirection ( DcMotor.Direction.REVERSE);
-            backRight.setDirection(DcMotor.Direction.REVERSE);
-        }
 
-    }*/
 
 }
