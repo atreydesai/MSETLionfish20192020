bd5c867863e20f76ed231895a99d84464af9098c
MatthewSong86 <54823079+MatthewSong86@users.noreply.github.com>
2019-11-20 18:22:43 -0800

    Update AutonCode2.java

--- AutonCode2.java
+++ AutonCode2.java
@@ -5,12 +5,13 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 
-@Autonomous(name="AutonCode2", group="LinearOpMode")
+@Autonomous(name="BluePlat", group="LinearOpMode")
 public class AutonCode2 extends LinearOpMode { //bluePlat
     private DcMotor frontLeft, backLeft, frontRight, backRight, claw, lift;
-    private Servo hooks1, hooks2, platform;
+    private Servo hooks1, hooks2, block;
     public int d = 4; //Diameter of Wheel
-    public double tick = 537.6; //# of ticks for one rotation
+    public double tick = 537.6; //# of ticks for one rotation\
+    public boolean left=true, right=false;
     public void runOpMode(){
         frontLeft  = hardwareMap.get(DcMotor.class, "frontLeft");
         frontRight  = hardwareMap.get(DcMotor.class, "frontRight");
@@ -28,9 +29,34 @@ public class AutonCode2 extends LinearOpMode { //bluePlat
         frontRight.setDirection ( DcMotor.Direction.FORWARD);
         backRight.setDirection(DcMotor.Direction.FORWARD);
         waitForStart();
-        forward(0);
+        forward(51);
+        hDown(); //maybe hUp();
+        backwards(51); //same as the first forward
+        hUp();
+        strafe(right,48);
 
 
+    }
+    public void bUp(){//hook servo up
+        block.setPosition(0);
+        telemetry.addData("Servo Power", block.getPosition());
+    }
+    public void bDown(){//hook servo down
+        block.setPosition(1);
+        telemetry.addData("Servo Power", block.getPosition ());
+    }
+    public void hUp(){//hook servo up
+        hooks1.setPosition(0);
+        hooks2.setPosition(1);
+        telemetry.addData("Servo Power", hooks1.getPosition());
+        telemetry.addData("Servo Power", hooks2.getPosition());
+
+    }
+    public void hDown(){//hook servo down
+        hooks1.setPosition(1);
+        hooks2.setPosition(0);
+        telemetry.addData("Servo Power", hooks1.getPosition());
+        telemetry.addData("Servo Power", hooks2.getPosition());
     }
     public int distanceCalc(int distance){
         int ticks=(int)(tick*(distance/(12.56)));
